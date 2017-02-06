6281845ae619753d47e7302b8dee1320e596e2d7
James Houghton <James.p.houghton@gmail.com>
2017-02-06 08:57:25 -0500

    refactoring to make robust

--- homer/parser.py
+++ homer/parser.py
@@ -1,16 +1,15 @@
-
 import dask.bag as db
 import glob
 
-import json
+import ujson
 from itertools import combinations
 import re
 import dateutil.parser
 from nltk.tokenize import RegexpTokenizer
 
-
 import os
 import pickle
+import pandas as pd
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -18,38 +17,17 @@ __location__ = os.path.realpath(
 sw_file = os.path.join(__location__, "stopwords.pickle")
 tw_stopwords = pickle.load(open(sw_file, "rb"))
 
+tokenizer = RegexpTokenizer(r'\w+')
 
-def get_message_cooccurences(json_message, hashtags_only=False):
-    """
-    Identifies word combinations in a single twitter message.
-    Drops stopwords and punctuation.
 
-    Parameters
-    ----------
-    json_message: basestring
-        a single line from a raw twitter json file
-
-    Returns
-    -------
-    sets: list of tuples
-        each tuple contains the date and two words (in alphabetical order) of a pair
-
-    """
-
-    # todo: make the hashtags_only parameter operable
-    try:
-        parsed_json = json.loads(json_message)
-        text = parsed_json['text'].lower()
-
-    except KeyError:  # There is no 'Text' in json body
-        return []
+def find_pairs(parsed_json):
+    text = parsed_json['text'].lower()
 
     # remove hyperlinks
     text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
 
     # tokenize, dropping punctuation
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(text)
+    tokens = tokenizer.tokenize(text)  # this should release GIL - be ok for parallelization
 
     # drop stopwords
     tokens = filter(lambda x: x not in tw_stopwords, tokens)
@@ -57,11 +35,63 @@ def get_message_cooccurences(json_message, hashtags_only=False):
     date = dateutil.parser.parse(parsed_json['created_at']).strftime("%Y%m%d")
 
     sets = [tuple([date] + sorted(pair)) for pair in combinations(tokens, 2)]
-
     return sets
 
 
-def get_weighted_edgelist(tw_files, hashtags_only=False):
+#
+# def get_message_cooccurences(json_message, languages=[], hashtags_only=False):
+#     """
+#     Identifies word combinations in a single twitter message.
+#     Drops stopwords and punctuation.
+#
+#     Parameters
+#     ----------
+#     json_message: basestring
+#         a single line from a raw twitter json file
+#
+#     Returns
+#     -------
+#     sets: list of tuples
+#         each tuple contains the date and two words (in alphabetical order) of a pair
+#
+#     """
+#
+#     # todo: make the hashtags_only parameter operable
+#     try:
+#         parsed_json = json.loads(json_message)
+#         assert parsed_json['lang'] in languages
+#         text = parsed_json['text'].lower()
+#
+#     except KeyError:  # There is no 'text', or no 'lang' in json body
+#         return []
+#
+#     except AssertionError:  # Not in the target language
+#         return []
+#
+#     # remove hyperlinks
+#     text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
+#
+#     # tokenize, dropping punctuation
+#     tokenizer = RegexpTokenizer(r'\w+')
+#     tokens = tokenizer.tokenize(text)
+#
+#     # drop stopwords
+#     tokens = filter(lambda x: x not in tw_stopwords, tokens)
+#
+#     date = dateutil.parser.parse(parsed_json['created_at']).strftime("%Y%m%d")
+#
+#     sets = [tuple([date] + sorted(pair)) for pair in combinations(tokens, 2)]
+#
+#     return sets
+#
+
+
+
+
+def get_weighted_edgelist(tw_file_globstring,
+                          languages,
+                          save_threshold=3,
+                          hashtags_only=False):
     """
 
     Parameters
@@ -75,37 +105,34 @@ def get_weighted_edgelist(tw_files, hashtags_only=False):
         weighted edgelist, columns ['Date', 'W1', 'W2', 'Count']
 
     """
+    messages = db.read_text(tw_file_globstring, compression='gzip').map(ujson.loads)
 
-    cooccurences = db.read_text(tw_files).map(get_message_cooccurences).concat()
-    frequencies = cooccurences.frequencies()
-    dicts = frequencies.map(
-        lambda x: {'Date': int(x[0][0]),
-                   'W1': str(x[0][1]),
-                   'W2': str(x[0][2]),
-                   'Count': int(x[1])})
-    df = dicts.to_dataframe()
+    def selector(msg):
+        return 'lang' in msg and 'text' in msg and msg['lang'] in languages
+
+    selection = messages.filter(selector)
+    sets = selection.map(find_pairs).concat()
+    frequencies = sets.frequencies()
+    over_threshold = frequencies.filter(lambda x: x[1] >= save_threshold)
+    expand = over_threshold.map(lambda x: (x[0][0], x[0][1], x[0][2], x[1]))
+
+    template = pd.DataFrame([{'Date': 20170121, 'W1': 'Toad', 'W2': u'Bug', 'Count': 21}],
+                            columns=['Date', 'W1', 'W2', 'Count'])
+
+    df = expand.to_dataframe(template)
+
     return df
 
 
 def build_weighted_edgelist_db(tw_file_globstring,
                                output_globstring,
-                               hashtags_only=False):
-    files = glob.glob(tw_file_globstring)  # 'tests/resources/testfile*.gz'
-    weighted_edgelist = get_weighted_edgelist(files,
+                               hashtags_only=False,
+                               languages=['en'],
+                               save_threshold=3):
+    weighted_edgelist = get_weighted_edgelist(tw_file_globstring,
+                                              languages=languages,
+                                              save_threshold=save_threshold,
                                               hashtags_only=hashtags_only)
 
-    weighted_edgelist.to_hdf(output_globstring, '/weighted_edge_list', dropna=True)  # 'working/testdb_*.hdf'
+    weighted_edgelist.to_hdf(output_globstring, '/weighted_edge_list', dropna=True)
     return weighted_edgelist
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,20 +5,21 @@ import dask
 class TestFileParsing(unittest.TestCase):
     def test_get_word_cooccurrences(self):
         """basic functionality """
-        from homer.parser import get_word_cooccurrences
+        from homer.homer.parser import get_message_cooccurences
 
         with open('resources/testfile.txt') as f:
             msg = f.readline()
 
-        sets = get_word_cooccurrences(msg)
+        sets = get_message_cooccurences(msg, languages=['en'])
         self.assertIsInstance(sets, list)
         self.assertIsInstance(sets[0], tuple)
         self.assertEqual(len(sets[0]), 3)
 
     def test_parse_tw_file(self):
         """basic functionality """
-        from homer.parser import get_weighted_edgelist
+        from homer.homer.parser import get_weighted_edgelist
 
-        df = get_weighted_edgelist('resources/testfile.txt')
+        df = get_weighted_edgelist(tw_files='resources/testfile.txt',
+                                   languages=['en'])
         self.assertIsInstance(df, dask.dataframe.DataFrame)
 
